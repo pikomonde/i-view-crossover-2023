aa772e208a5d54ac1d216362b86106c2f8bd0c71
pikomonde <32364823+pikomonde@users.noreply.github.com>
2023-08-26 20:26:36 +0700

    update-01

--- index.js
+++ index.js
@@ -8,7 +8,28 @@ const DEFAULT_BALANCE = 100;
 const MAX_EXPIRATION = 60 * 60 * 24 * 30;
 const memcachedClient = new memcached(`${process.env.ENDPOINT}:${process.env.PORT}`);
 exports.chargeRequestRedis = async function (input) {
+    var responses = [
+        chargeRedisReq(),
+        chargeRedisReq(),
+        chargeRedisReq(),
+        chargeRedisReq(),
+        chargeRedisReq(),
+        chargeRedisReq(),
+    ]
+    var awaitResponses = [
+        await responses[0],
+        await responses[1],
+        await responses[2],
+        await responses[3],
+        await responses[4],
+        await responses[5],
+    ]
+    printResponses(awaitResponses);
+    return null;
+};
+async function chargeRedisReq() {
     const redisClient = await getRedisClient();
+    var startTime = new Date().getTime();
     var remainingBalance = await getBalanceRedis(redisClient, KEY);
     var charges = getCharges();
     const isAuthorized = authorizeRequest(remainingBalance, charges);
@@ -17,14 +38,19 @@ exports.chargeRequestRedis = async function (input) {
             remainingBalance,
             isAuthorized,
             charges: 0,
+            startTime,
+            timeElapsed: (new Date().getTime() - startTime),
         };
     }
     remainingBalance = await chargeRedis(redisClient, KEY, charges);
+    var timeElapsed = (new Date().getTime() - startTime);
     await disconnectRedis(redisClient);
     return {
         remainingBalance,
         charges,
         isAuthorized,
+        startTime,
+        timeElapsed,
     };
 };
 exports.resetRedis = async function () {
@@ -54,7 +80,28 @@ exports.resetMemcached = async function () {
     return ret;
 };
 exports.chargeRequestMemcached = async function (input) {
+    var responses = [
+        chargeMemcachedReq(),
+        chargeMemcachedReq(),
+        chargeMemcachedReq(),
+        chargeMemcachedReq(),
+        chargeMemcachedReq(),
+        chargeMemcachedReq(),
+    ]
+    var awaitResponses = [
+        await responses[0],
+        await responses[1],
+        await responses[2],
+        await responses[3],
+        await responses[4],
+        await responses[5],
+    ]
+    printResponses(awaitResponses);
+    return null;
+};
+async function chargeMemcachedReq() {
     var remainingBalance = await getBalanceMemcached(KEY);
+    var startTime = new Date().getTime();
     const charges = getCharges();
     const isAuthorized = authorizeRequest(remainingBalance, charges);
     if (!authorizeRequest(remainingBalance, charges)) {
@@ -62,6 +109,8 @@ exports.chargeRequestMemcached = async function (input) {
             remainingBalance,
             isAuthorized,
             charges: 0,
+            startTime,
+            timeElapsed: (new Date().getTime() - startTime),
         };
     }
     remainingBalance = await chargeMemcached(KEY, charges);
@@ -69,8 +118,37 @@ exports.chargeRequestMemcached = async function (input) {
         remainingBalance,
         charges,
         isAuthorized,
+        startTime,
+        timeElapsed: (new Date().getTime() - startTime),
     };
 };
+function printResponses(responses) {
+    responses.sort(function(a, b){return a.startTime - b.startTime})
+    var sum = 0;
+    var min = 2000;
+    var max = -1;
+    responses.forEach(response => {
+        console.log(
+            "remainingBalance: " + response.remainingBalance + ", " +
+            "isAuthorized: " + response.isAuthorized + ", " +
+            "charges: " + response.charges + ", " +
+            "startTime: " + response.startTime + ", " +
+            "timeElapsed: " + response.timeElapsed
+        );
+        sum += response.timeElapsed;
+        if (response.timeElapsed < min) {
+            min = response.timeElapsed;
+        }
+        if (response.timeElapsed > max) {
+            max = response.timeElapsed;
+        }
+    })
+    var average = sum / responses.length;
+    console.log("sum: " + sum)
+    console.log("min: " + min)
+    console.log("max: " + max)
+    console.log("average: " + average)
+}
 async function getRedisClient() {
     return new Promise((resolve, reject) => {
         try {
@@ -108,13 +186,14 @@ function authorizeRequest(remainingBalance, charges) {
     return remainingBalance >= charges;
 }
 function getCharges() {
-    return DEFAULT_BALANCE / 20;
+    return 23;
 }
 async function getBalanceRedis(redisClient, key) {
     const res = await util.promisify(redisClient.get).bind(redisClient).call(redisClient, key);
     return parseInt(res || "0");
 }
 async function chargeRedis(redisClient, key, charges) {
+    // await new Promise(r => setTimeout(r, 100));
     return util.promisify(redisClient.decrby).bind(redisClient).call(redisClient, key, charges);
 }
 async function getBalanceMemcached(key) {
@@ -130,6 +209,7 @@ async function getBalanceMemcached(key) {
     });
 }
 async function chargeMemcached(key, charges) {
+    // await new Promise(r => setTimeout(r, 100));
     return new Promise((resolve, reject) => {
         memcachedClient.decr(key, charges, (err, result) => {
             if (err) {
